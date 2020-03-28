1ba47fc513b2ee85b9399c15e31e155f45756e73
Luiz Paulo Oliveira <luizpaulodev@gmail.com>
2020-03-28 01:06:12 -0300

    Removendo comentários

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,23 +10,3 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333);
-
-/**
-* Rota / Recurso
-*/
-
-/**
-* Métodos Http
-* 
-* GET: Buscar/Listar uma informação do back-end
-* POST: Criar uma informação no back-end
-* PUT: Alterar uma informação no back-end
-* DELETE: Deltar uma informação no back-end
-*/
-
-/**
-* Tipos de parâmetros:
-* 	Query Params: Parâmetros nomeados enviados na rota após o "?" (Filtros, Paginação)
-*   Route Params: Parâmetros utilizados para identificar recursos
-*   Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
-*/
