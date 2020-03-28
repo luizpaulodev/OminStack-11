a7044cb7edd9a50efbd5d5565c3388eb4ba5bffe
Luiz Paulo Oliveira <luizpaulodev@gmail.com>
2020-03-28 13:36:29 -0300

    Criação das páginas

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+
+import './styles.css';
+
+import heroesimg from '../../assets/heroes.png';
+import logoimg from '../../assets/logo.svg';
+
+export default function Logon(props) {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoimg} alt="Be The Hero"/>
+
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="Sua ID" type="text"/>
+
+          <button type="submit" className="button">Entrar</button>
+
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#E02041"/>
+            Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+
+      <img src={heroesimg} alt="Heroes"/>
+    </div>
+  );
+}
